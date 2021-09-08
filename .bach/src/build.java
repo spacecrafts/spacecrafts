6adb9a881d1d1af2b47971e24ce23316ccbb4ac4
Christian Stein <sormuras@gmail.com>
2021-09-08 20:09:09 +0200

    Upgrade to Bach 17-M2

--- .bach/src/build.java
+++ .bach/src/build.java
@@ -1,19 +1,22 @@
-import com.github.sormuras.bach.*;
+import com.github.sormuras.bach.Bach;
+import com.github.sormuras.bach.conventional.ConventionalSpace;
 import com.github.sormuras.bach.external.JUnit;
 
 class build {
-
   public static void main(String... args) {
     try (var bach = new Bach(args)) {
       var grabber = bach.grabber(JUnit.version("5.8.0-RC1"));
 
-      var main = bach.builder().conventionalSpace("main", "se.jbee.spacecrafts", "se.jbee.turnmaster");
-      main.compile(javac -> javac.with("-Xlint"), jar -> jar.with("--verbose"));
+      var main =
+          ConventionalSpace.of(bach, "main")
+              .modulesAddModule("se.jbee.spacecrafts")
+              .modulesAddModule("se.jbee.turnmaster");
+      main.compile(javac -> javac.add("-Xlint"), jar -> jar.verbose(true));
 
       bach.logCaption("Perform automated checks");
-      var test = main.dependentSpace("test", "test.integration");
+      var test = main.newDependentConventionalSpace("test").modulesAddModule("test.integration");
       test.grab(grabber, "org.junit.jupiter", "org.junit.platform.console");
-      test.compile(javac -> javac.with("-g").with("-parameters"));
+      test.compile(javac -> javac.add("-g").add("-parameters"), jar -> jar);
       test.runAllTests();
     }
   }
